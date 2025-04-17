76d077fea2eeec6a08f271a6aa546b0c96dcf477
waku <wakunguma13@gmail.com>
2025-04-17 10:32:51 -0600

    added `attributeNames()` and `textContent()` methods on html element class

--- main.ts
+++ main.ts
@@ -3,17 +3,39 @@ import { ElementHandle } from "puppeteer";
 import {launch, Browser} from "puppeteer";
 
 
-async function getAttributeNames(element: ElementHandle,page:Page):Promise<string[]>{
-	const attrs = await page.evaluate(element => {
-		return element.getAttributeNames()
-	},element)
 
-	return attrs
-}
 
 class HtmlElement{
-	constructor(){
+	element: ElementHandle
+	page: Page
 
+	constructor(element: ElementHandle,page: Page){
+		this.element = element
+		this.page = page
+	}
+
+	/** Get the attribute name of the html element e.g class, id, aria-label
+	 *  as an array of strings
+	 * @returns {string[]} An array of attribute names
+	*/
+	async attributeNames():Promise<string[]>{
+		const attrs = await this.page.evaluate(element => {
+			return element.getAttributeNames()
+		},this.element)
+	
+		return attrs
+	}
+
+	/** Get the attribute name of the html element e.g class, id, aria-label
+	 *  as an array of strings
+	 * @returns {string[]} An array of attribute names
+	*/
+	async textContent():Promise<string>{
+		const text = await this.page.evaluate(element => {
+			return element.textContent
+		},this.element)
+	
+		return text
 	}
 }
 
@@ -22,9 +44,11 @@ async function main(){
 	const page = await browser.newPage()
 	await page.goto("http://localhost:5173")
 	
-	const button = await page.locator('h1').waitHandle()
-	const attrs = await getAttributeNames(button,page)
-	console.log(attrs)
+	const buttonHandle = await page.locator('button').waitHandle()
+	const button = new HtmlElement(buttonHandle,page)
+	const attrs = await button.attributeNames()
+	const text = await button.textContent()
+	console.log(text)
 
 	await browser.close()
 }
