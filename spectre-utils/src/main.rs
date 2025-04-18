3f8069a4748a340439b7608a282db5deaa598e43
waku <wakunguma13@gmail.com>
2025-04-18 17:51:33 -0600

    organize code into workspace

---
+++ spectre-utils/src/main.rs
@@ -0,0 +1,46 @@
+mod error;
+pub use error::Error;
+use futures_util::{StreamExt, future, pin_mut};
+use serde::{Deserialize, Serialize};
+use serde_json::Value;
+use std::{
+    io::{Cursor, Read},
+    process::{Child, Command},
+};
+use tokio_tungstenite::{connect_async, tungstenite::http::response};
+
+async fn download_chrome() -> Result<(), Error> {
+    println!("Downloading chrome...");
+    let url = "https://storage.googleapis.com/chrome-for-testing-public/135.0.7049.95/win64/chrome-win64.zip";
+    let response = reqwest::get(url).await?;
+
+    let bytes = response.bytes().await?;
+    let cursor = Cursor::new(bytes);
+    let mut archive = zip::ZipArchive::new(cursor)?;
+
+    println!("Unzipping...");
+
+    for i in 0..archive.len() {
+        let mut file = archive.by_index(i)?;
+        // FIXME
+        let outpath = file.enclosed_name().unwrap();
+
+        if file.is_dir() {
+            std::fs::create_dir_all(outpath)?;
+        } else {
+            if let Some(p) = outpath.parent() {
+                if !p.exists() {
+                    std::fs::create_dir_all(p)?;
+                }
+            }
+            let mut out_file = std::fs::File::create(&outpath)?;
+            std::io::copy(&mut file, &mut out_file)?;
+        }
+    }
+
+    Ok(())
+}
+
+fn main() {
+    println!("Hello, world!");
+}
--- spectre/src/lib.rs
+++ spectre/src/lib.rs
@@ -9,38 +9,6 @@ use std::{
 };
 use tokio_tungstenite::{connect_async, tungstenite::http::response};
 
-async fn download_chrome() -> Result<(), Error> {
-    println!("Downloading chrome...");
-    let url = "https://storage.googleapis.com/chrome-for-testing-public/135.0.7049.95/win64/chrome-win64.zip";
-    let response = reqwest::get(url).await?;
-
-    let bytes = response.bytes().await?;
-    let cursor = Cursor::new(bytes);
-    let mut archive = zip::ZipArchive::new(cursor)?;
-
-    println!("Unzipping...");
-
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i)?;
-        // FIXME
-        let outpath = file.enclosed_name().unwrap();
-
-        if file.is_dir() {
-            std::fs::create_dir_all(outpath)?;
-        } else {
-            if let Some(p) = outpath.parent() {
-                if !p.exists() {
-                    std::fs::create_dir_all(p)?;
-                }
-            }
-            let mut out_file = std::fs::File::create(&outpath)?;
-            std::io::copy(&mut file, &mut out_file)?;
-        }
-    }
-
-    Ok(())
-}
-
 pub struct Browser {
     process: Child,
 }
@@ -87,10 +55,4 @@ impl Drop for Browser {
             .kill()
             .expect("Process should have been killed");
     }
-}
-
-#[tokio::main]
-async fn main() -> Result<(), Error> {
-    let browser = Browser::launch().await?;
-    Ok(())
 }
