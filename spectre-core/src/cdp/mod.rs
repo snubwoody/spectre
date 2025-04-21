ada7dad6e4167143892cbf65a2a5c47071285670
waku <wakunguma13@gmail.com>
2025-04-21 11:25:17 -0600

    refactor: make all response types clone

--- spectre-core/src/cdp/mod.rs
+++ spectre-core/src/cdp/mod.rs
@@ -135,47 +135,49 @@ pub type PageNavigateResponse = CDPResponse<PageNavigateBody>;
 pub type GetDocumentResponse = CDPResponse<GetDocumentBody>;
 
 #[derive(Debug, Deserialize, Serialize)]
-pub struct CDPResponse<T> {
+pub struct CDPResponse<T:Clone> {
     id: i32,
     #[serde(rename = "sessionId")]
     session_id: Option<String>,
     result: T,
 }
 
-impl<T> CDPResponse<T> {
+impl<T> CDPResponse<T> 
+where T: Clone
+{
     pub fn id(&self) -> i32 {
         self.id
     }
 
-    pub fn body(self) -> T {
-        self.result
+    pub fn body(&self) -> T {
+        self.result.clone()
     }
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct CreateTargetBody {
     pub target_id: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Clone)]
 pub struct GetTargetBody {
     #[serde(rename = "targetInfos")]
     pub targets: Vec<Target>,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct AttachToTargetBody {
     pub session_id: String,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Clone)]
 pub struct GetDocumentBody {
     pub root: DomNode,
 }
 
-#[derive(Debug, Serialize, Deserialize)]
+#[derive(Debug, Serialize, Deserialize,Clone)]
 #[serde(rename_all = "camelCase")]
 pub struct PageNavigateBody {
     pub frame_id: String,
