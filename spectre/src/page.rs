50a4d09bf946faa5277863113c7c84d20d7281ca
waku <wakunguma13@gmail.com>
2025-04-27 10:48:50 -0600

    refactor: rename `Page::get_by_class` to `Page::locate_by_class`

--- spectre/src/page.rs
+++ spectre/src/page.rs
@@ -57,7 +57,24 @@ impl Page {
         Ok(url.to_owned())
     }
 
-    pub async fn get_by_class(&self, class: &str) -> Result<Option<Element>> {
+	/// Locate the first element whose class name matches
+	/// the input class.
+	/// 
+	/// # Example
+	/// ```
+	/// use spectre::{Result,Page,Browser};
+	/// 
+	/// #[tokio::main]
+	/// async fn main() -> Result<()>{
+	///     let mut browser = Browser::start().await?;
+	///     let page = browser.new_page().await?;
+	/// 
+	///     let element = page.locate_by_class("does-not-exist").await?;
+	///     assert!(element.is_none());
+	///     Ok(())
+	/// }
+	/// ```
+    pub async fn locate_by_class(&self, class: &str) -> Result<Option<Element>> {
         let root = self.get_dom().await?;
         let element = self
             .session
