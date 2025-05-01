8330d29934c1d859aea089bcce4dc11e25482f1e
waku <wakunguma13@gmail.com>
2025-04-30 23:14:06 -0600

    feat: reuse browser in `spectre::test` macro

--- spectre-macros/src/lib.rs
+++ spectre-macros/src/lib.rs
@@ -2,7 +2,7 @@ use proc_macro::TokenStream;
 use quote::quote;
 use syn::ItemFn;
 
-/// Create a page for every test using one browser
+/// Create a page for every test using one browser.
 ///
 /// ```ignore
 /// #[spectre::test]
@@ -18,21 +18,39 @@ pub fn test(_: TokenStream, input: TokenStream) -> TokenStream {
     let sig = &input_fn.sig;
     let attrs = &input_fn.attrs;
     let block = &input_fn.block;
+
     let ident = &sig.ident;
+    let output = &sig.output;
 
     if sig.asyncness.is_none() {
         return syn::Error::new_spanned(
             sig.fn_token,
-            "the `async` keyword is missing from the function declaration",
+            "the `async` keyword is missing from the function declaration, functions marked with `spectre::test` must be async",
         )
         .to_compile_error()
         .into();
     }
 
+    let _ = dotenv::dotenv();
+    let var = std::env::var("SPECTRE_TEST_PORT").expect("please set `SPECTRE_TEST_PORT`");
+    let port:u16 = var.parse().unwrap();
+    
     quote! {
         #[tokio::test]
         #(#attrs)*
-        #vis async fn #ident(){
+        #vis async fn #ident() #output {
+            let is_running = spectre::Browser::is_running(#port).await;
+
+            let mut browser = if is_running{
+                spectre::Browser::connect(#port).await.unwrap()
+            }else{
+                spectre::Browser::start_on(#port).await.unwrap()
+            };
+
+            browser.kill_on_drop(false);
+
+            // compile_error!("Failed to compile");
+            let mut page = browser.new_page().await.unwrap();
             #block
         }
     }
